b6d03631f7058cc71957e93ca496d38cdfc51b27
Yulin Wu <drm343@gmail.com>
2019-11-13 01:20:34 +0800

    Modify script for slackware.

--- desks/distro-mode.sh
+++ desks/distro-mode.sh
@@ -5,21 +5,31 @@
 
 # Remove distro command
 Remove () {
-    local result=`find $SIMPLE_CONFIG/bin -type l`
-    for i in $result;
-    do
-        result=`ls -l $i | grep "$DISTRO"`;
-        if [ ! -z "$result" ]; then
-            rm $i;
-        fi
-    done
+    Remove_Local () {
+        local result=$@
+        for i in $result;
+        do
+            result=`ls -l $i | grep "$DISTRO"`;
+            if [ ! -z "$result" ]; then
+                rm $i;
+            fi
+        done
+    }
+
+    Remove_Local `find $SIMPLE_CONFIG/bin -type l`
+    Remove_Local `find $SIMPLE_CONFIG/completion -type l`
 }
 
 # Install distro command
 Install () {
-    pushd $SIMPLE_CONFIG/bin
-    ln -s $SIMPLE_CONFIG/distro/$DISTRO/* .
-    popd
+    Install_Local () {
+        pushd $SIMPLE_CONFIG/$1
+        ln -s $SIMPLE_CONFIG/distro/$DISTRO/$1/* .
+        popd
+    }
+
+    Install_Local completion
+    Install_Local bin
 }
 
 
---
+++ distro/slackware/completion/git-completion.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+source /usr/doc/git-2.23.0/contrib/completion/git-completion.bash
