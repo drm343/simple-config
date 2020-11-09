c34c4a03c967ab73d03b7031deed967edcb52e30
Yulin Wu <drm343@gmail.com>
2020-11-09 13:43:15 +0800

    Fix reload menu and run py3 venv with desk.

--- plugin/fuzzy-menu.plugin.bash
+++ plugin/fuzzy-menu.plugin.bash
@@ -5,9 +5,12 @@ fuzzy-menu () {
 
     local command_mode=`echo "$command" | grep "mode"`
     local menu_mode=`echo "$command" | grep "\.menu"`
+    local py3_mode=`echo "$command" | grep "py3-*"`
 
     if [ ! -z "$command_mode" ]; then
         desk go $command_mode
+    elif [ ! -z "$py3_mode" ]; then
+        desk go $py3_mode
     elif [ ! -z "$menu_mode" ]; then
         $(cat $SIMPLE_CONFIG/data/$menu_mode | fzy)
     else
--- plugin/reload-menu.plugin.bash
+++ plugin/reload-menu.plugin.bash
@@ -21,8 +21,7 @@ reload-menu () {
             $SIMPLE_CONFIG/data/*.menu \
             $SIMPLE_CONFIG/desks/* \
             | sed "s/.*desks\/\(.*\)\.sh/\1/" \
-            | sed 's/\x1b\[[0-9;]*m//g' \
-            | sed 's/\x1b\[[0m//g')
+            | sed 's/\x1b\[[0-9;]*m//g')
 
         echo -en "reload-menu
 " >> $SIMPLE_CONFIG/data/menu
