b00fbee791a8c06cf11f5df873c008b01b07b522
Yulin Wu <drm343@gmail.com>
2022-02-16 14:09:02 +0800

    Fix bash5 bug about ls --color.

--- alias.sh
+++ alias.sh
@@ -1,4 +1,4 @@
-alias ls='ls --color'
+alias ls='ls --color=auto'
 alias editor='vim'
 alias stripcolors='sed "s/\x1B\[\([0-9]\{1,2\}\(;[0-9]\{1,2\}\)\?\)\?[mGK]//g"'
 source $SIMPLE_CONFIG/lib/fix-bash-syntax
